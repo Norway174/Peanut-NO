a17e7988bef05280e3c6a115c66ba94598623c30
Norway174 <willyodden@hotmail.com>
2019-07-18 14:50:32 +0200

    Create config_example.js

---
+++ config_example.js
@@ -0,0 +1,11 @@
+const config = {
+
+	// NOTICE: Make a copy of this file, and rename it to "config.js".
+	// And change out the values below with your own.
+	
+	// Bot token - Super secret! - Available on https://discordapp.com/developers/applications/me
+	"token":"SUPERSECRETBOTTOKENHERE"
+
+};
+
+module.exports = config;
